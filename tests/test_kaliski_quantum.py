ba46541c8a9839fcdba87c791123e86bd005de7a
Diego Polimeni <diego.polimeni@epfl.ch>
2024-11-08 18:28:09 +0100

    added tests for kaliski, doubling and addition

--- tests/test_kaliski_quantum.py
+++ tests/test_kaliski_quantum.py
@@ -2,18 +2,21 @@ import src.quantum.ec_arithmetic as qECarithm
 import pytest
 import qrisp
 
-primes = [3, 5, 7, 11, 13, 17, 19, 23]
+primes = [
+    3,
+    5,
+]  # 7, 11, 13, 17, 19, 23]
 
-@pytest.mark.parametrize("p", primes)
+
 @pytest.mark.parametrize("v_cl", range(1, 23))
-
-def test_kaliski_quantum(v, p):
+@pytest.mark.parametrize("p", primes)
+def test_kaliski_quantum(v_cl, p):
 
     if v_cl >= p:
         pytest.skip()
 
     try:
-        expected_inverse = pow(v, -1, p)
+        expected_inverse = pow(v_cl, -1, p)
     except ValueError:
         pytest.skip(f"No modular inverse exists for v={v_cl}, p={p} (non-coprime)")
 
@@ -22,5 +25,6 @@ def test_kaliski_quantum(v, p):
     m = qrisp.QuantumArray(qtype=qrisp.QuantumBool(), shape=(2 * p.bit_length(),))
 
     quantum_inverse = qECarithm.kaliski_quantum(v, p, m)
-
-    assert quantum_inverse == expected_inverse, f"Quantum inverse {quantum_inverse} did not match expected {expected_inverse} for v={v}, p={p}"
+    assert quantum_inverse.get_measurement() == {
+        expected_inverse: 1
+    }, f"Quantum inverse {quantum_inverse} did not match expected {expected_inverse} for v={v}, p={p}"
