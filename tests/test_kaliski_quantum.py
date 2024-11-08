9d3a5a0ba194c22ac082fd746b03d4fc37b448e4
Diego Polimeni <diego.polimeni@epfl.ch>
2024-11-08 15:01:55 +0100

    implement test pipeline

---
+++ tests/test_kaliski_quantum.py
@@ -0,0 +1,26 @@
+import src.quantum.ec_arithmetic as qECarithm
+import pytest
+import qrisp
+
+primes = [3, 5, 7, 11, 13, 17, 19, 23]
+
+@pytest.mark.parametrize("p", primes)
+@pytest.mark.parametrize("v_cl", range(1, 23))
+
+def test_kaliski_quantum(v, p):
+
+    if v_cl >= p:
+        pytest.skip()
+
+    try:
+        expected_inverse = pow(v, -1, p)
+    except ValueError:
+        pytest.skip(f"No modular inverse exists for v={v_cl}, p={p} (non-coprime)")
+
+    v = qrisp.QuantumModulus(p)
+    v[:] = v_cl
+    m = qrisp.QuantumArray(qtype=qrisp.QuantumBool(), shape=(2 * p.bit_length(),))
+
+    quantum_inverse = qECarithm.kaliski_quantum(v, p, m)
+
+    assert quantum_inverse == expected_inverse, f"Quantum inverse {quantum_inverse} did not match expected {expected_inverse} for v={v}, p={p}"
